be673a5325114eb91658a0e7c06321c699e68992
Shalabh Aggarwal <shalabh7777@gmail.com>
2022-12-29 16:28:53 +0530

    Added Chapter 4

---
+++ Chapter-4/my_app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+app.secret_key = 'some_random_key'
+
+from my_app.catalog.views import catalog
+app.register_blueprint(catalog)
+
+with app.app_context():
+    db.create_all()
